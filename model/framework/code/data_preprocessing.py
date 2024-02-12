29496c7f6c070846c96b8a9b940afee00e242cac
leilayesufu <leilayesufu2002@gmail.com>
2024-02-12 17:08:17 +0100

    commit

--- model/framework/code/data_preprocessing.py
+++ model/framework/code/data_preprocessing.py
@@ -5,6 +5,12 @@ from rdkit.Chem import AllChem
 from rdkit.Chem import DataStructs
 from rdkit.Chem import QED
 from sklearn.preprocessing import StandardScaler
+import pickle
+import os
+
+# Define paths
+root = os.path.dirname(os.path.abspath(__file__))
+checkpoints_dir = os.path.abspath(os.path.join(root, "..", "..", "checkpoints"))
 
 class DataProcessor:
     def __init__(self):
@@ -31,12 +37,13 @@ class DataProcessor:
         qe_df = pd.DataFrame(qe)
         return qe_df
 
-    def scale_qed(self, df):
-        scaler = StandardScaler()
-        scaled_array = scaler.fit_transform(df)
-        scaled_df = pd.DataFrame(scaled_array, columns=df.columns)
-        return scaled_df
+    # Function to load scaler from a pickle file
+    def load_scaler(self, scaler_filepath):
+        with open(scaler_filepath, 'rb') as f:
+            scaler = pickle.load(f)
+        return scaler
 
+        
     def preprocess_data(self, df):
         # Detect the column containing SMILES strings
         smiles_column = df.columns[df.apply(lambda col: col.apply(lambda x: Chem.MolFromSmiles(str(x)) is not None)).all()][0]
@@ -48,10 +55,19 @@ class DataProcessor:
         # Calculate QED descriptors
         qed_df = self.qed_calculation(smiles)
         
-        # Scale the QED descriptors
-        scaled_qed_df = self.scale_qed(qed_df)
+        # Load scaler from pickle file
+        scaler = self.load_scaler(scaler_filepath)
+
+        # Scale QED descriptors using the loaded scaler
+        scaled_array = scaler.transform(qed_df)
+        scaled_qed_df = pd.DataFrame(scaled_array, columns=qed_df.columns)
         
         # Concatenate the descriptor dataframes
         processed_df = pd.concat([ecfp_df, scaled_qed_df], axis=1)
         
         return processed_df
+
+
+
+# Define scaler file path
+scaler_filepath = os.path.join(checkpoints_dir, "scaler.pkl")
